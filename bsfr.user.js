93d2758b3c783d620483175185ff1551c9da56c7
Hei5enberg44 <antoineboulet44@gmail.com>
2022-09-10 22:57:18 +0200

    Version initiale

---
+++ bsfr.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Z/Place - Logo BSFR
+// @namespace    https://github.com/Hei5enberg44/zplace-bsfr
+// @version      0.1
+// @description  Overlay logo BSFR.
+// @author       Hei5enberg
+// @match        https://place.zevent.fr
+// @icon         https://raw.githubusercontent.com/Hei5enberg44/zplace-bsfr/main/icon.jpg
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/Hei5enberg44/zplace-bsfr/main/bsfr.user.js
+// @updateURL    https://raw.githubusercontent.com/Hei5enberg44/zplace-bsfr/main/bsfr.user.js
+// @supportURL   https://github.com/Hei5enberg44/zplace-bsfr/issues
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const container = document.querySelector('.react-transform-component')
+    const img = document.createElement('img')
+    img.style.position = 'absolute'
+    img.style.top = 0
+    img.style.left = 0
+    img.style.width = '50px'
+    img.style.height = '50px'
+    img.style.zIndex = 1001
+    img.style.opacity = 0.75
+    img.src = 'https://github.com/Hei5enberg44/zplace-bsfr/raw/main/BSFR.png'
+
+    setTimeout(function() {
+        const target = document.querySelector('.target')
+        target.style.zIndex = 1002
+        container.append(img)
+    }, 2000)
+})();
