4436be2a102bb603dee00806d2be54a7730bb24e
Hei5enberg44 <antoineboulet44@gmail.com>
2022-09-11 15:00:22 +0200

    Mise à jour de la version

--- bsfr.user.js
+++ bsfr.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Z/Place - Logo BSFR
 // @namespace    https://github.com/Hei5enberg44/zplace-bsfr
-// @version      0.2
+// @version      0.3
 // @description  Overlay logo BSFR.
 // @author       Hei5enberg
 // @match        https://place.zevent.fr
